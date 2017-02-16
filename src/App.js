ac3731379835561f1b1e0f87ef8c483a5620bcff
Christopher King <chrisking1@gmail.com>
2017-02-16 14:33:52 -0500

    add basic form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class FormField extends Component {
+  render() {
+    return (
+      <div className="formField" id="nameForm">
+        <form action ={this.props.action}>
+          {this.props.label}<input type="text"/><br/><br/>
+          <input type="submit"/>
+        </form>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>
-          App works!
-        </p>
+        <h1>
+          Does Austin have your unclaimed property?
+        </h1>
+        <FormField action="#" label="Enter your name: " />
       </div>
     );
   }
